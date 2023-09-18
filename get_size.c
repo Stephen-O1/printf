6e591481cddadd1877f0752ea7905a545d2b98f0
rumonah <rummaloba@gmail.com>
2023-09-18 07:58:07 -0700

    get_size.c

--- get_size.c
+++ get_size.c
@@ -12,8 +12,8 @@ int get_size(const char *format, int *i)
 	int curr_i = *i + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'i')
-		size = S LONG;
+	if (format[curr_i] == 'l')
+		size = S_LONG;
 	else if (format[curr_i] == 'h')
 		size = S_SHORT;
 
